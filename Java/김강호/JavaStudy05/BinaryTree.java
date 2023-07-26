561fed59f395201ceff4178579a5bfdb178e34c8
Kim Kang Ho <63511273+tgb02087@users.noreply.github.com>
2023-07-27 05:58:10 +0900

    Update BinaryTree.java

--- Java/김강호/JavaStudy05/BinaryTree.java
+++ Java/김강호/JavaStudy05/BinaryTree.java
@@ -31,4 +31,23 @@ public class BinaryTree {
         }
         size++;
     }
+    public static void bfs(Node node) {
+        Queue<Node> que = new ArrayDeque<>();
+        que.add(node);
+        while(!que.isEmpty()) {
+            Node now = que.poll();
+            System.out.println(now.value);
+            if(now.left!=null) que.add(now.left);
+            if(now.right!=null) que.add(now.right);
+        }
+    }
+    public static void dfs(Node node) {
+        //왼쪽
+        if(node.left!=null) dfs(node.left);
+        //루트
+        System.out.println(node.value);
+        //오른쪽
+        if(node.right!=null) dfs(node.right);
+    }
 }
+
